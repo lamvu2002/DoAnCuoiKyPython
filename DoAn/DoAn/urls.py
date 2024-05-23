964b944c4f597c99aa470773f66661f4a09bd10b
JustAlex <greenhornetnova3@gmail.com>
2024-05-23 09:40:22 +0700

    Update urls.py

--- DoAn/DoAn/urls.py
+++ DoAn/DoAn/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("qlbh/", include("qlbh.urls")),
+    path("", include("qlbh.urls")),
     path("admin/", admin.site.urls),
 ]
