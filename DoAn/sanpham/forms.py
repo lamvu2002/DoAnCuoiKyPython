e1c8db42bd7e9fcaf1bc0fcadf1f32c954a22cb0
Nguyen Lam Vu <20520867@gm.uit.edu.vn>
2024-06-09 13:19:49 +0700

    edit sanpham form

--- DoAn/sanpham/forms.py
+++ DoAn/sanpham/forms.py
@@ -2,20 +2,41 @@ from django import forms
 from .models import Sanpham
 
 
+from django import forms
+from .models import Sanpham
+
+
 class SanphamForm(forms.ModelForm):
+    DVT_CHOICES = [
+        ('cay', 'cay'),
+        ('hop', 'hop'),
+        ('cai', 'cai'),
+        ('quyen', 'quyen'),
+        ('chuc', 'chuc'),
+    ]
+    dvt = forms.ChoiceField(choices=DVT_CHOICES, label='Đơn Vị Tính')
+
     class Meta:
         model = Sanpham
         fields = '__all__'
         labels = {
             'masp': 'Mã Sản Phẩm',
             'tensp': 'Tên Sản Phẩm',
-            'dvt': 'Đơn Vị Tính',
             'nuocsx': 'Nước Sản Xuất',
             'gia': 'Giá',
         }
 
 
 class SanphamEditForm(forms.ModelForm):
+    DVT_CHOICES = [
+        ('cay', 'cay'),
+        ('hop', 'hop'),
+        ('cai', 'cai'),
+        ('quyen', 'quyen'),
+        ('chuc', 'chuc'),
+    ]
+    dvt = forms.ChoiceField(choices=DVT_CHOICES, label='Đơn Vị Tính')
+    
     class Meta:
         model = Sanpham
         fields = '__all__'
